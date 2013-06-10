732487fd611d4088effe47d1d073074c249b311e
André Hoffmann <antihero2006@gmail.com>
2013-06-10 15:44:30 +0200

    Fixed ROI sphere radius calculation

--- rstools/rsmathutils.c
+++ rstools/rsmathutils.c
@@ -140,13 +140,13 @@ double rsDistance(FloatPoint3D A, FloatPoint3D B)
             pow(A.y-B.y,2.0) +
             pow(A.z-B.z,2.0)
         ),
-        1.0/3.0
+        1.0/2.0
     );
 }
 
 BOOL rsVoxelInSphere(FloatPoint3D point, FloatPoint3D center, double radius)
 {
-    return rsDistance(point, center) < radius;
+    return rsDistance(point, center) <= radius;
 }
 
 BOOL rsVoxelInCube(FloatPoint3D point, FloatPoint3D center, FloatPoint3D dim)
