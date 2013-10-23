4ac36b3400b03537639b594d12c2ad802fce4ced
André Hoffmann <antihero2006@gmail.com>
2013-10-23 22:24:16 +0200

    rsmotionscrubbing2 working

--- rstools/rsmotionscrubbing2.c
+++ rstools/rsmotionscrubbing2.c
@@ -31,7 +31,30 @@ void rsMaskPrintHelp() {
 	printf(
 		"   -rp <rp.txt>           : the file containing the realignment parameters\n"
 	);
+	
+	printf(
+		"   -dvars <dvars.txt>     : (optional) file where the DVARS values will be\n"
+		"                            saved to\n"
+	);
     
+	printf(
+		"   -fd <fd.txt>           : (optional) file to which the framewise displacement\n"
+		"                            will be saved to\n"
+	);
+	
+	printf(
+		"   -flagged <flagged.txt> : (optional) file to which the indices of all\n"
+		"                            flagged frames will be saved to\n"
+	);
+	
+	printf(
+		"   -dvarsthreshold <float>: (optional) DVARs threshold\n"
+	);
+	
+	printf(
+		"   -fdthreshold <float>   : (optional) framewise displacement threshold\n"
+	);
+	
     printf(
         "   -mask <mask>           : a mask specifying the ROI\n"
     );
@@ -50,6 +73,14 @@ extern inline double deg2mm(const double dist, const double deg) {
 	return M_PI * dist * (deg/180.0);
 }
 
+extern inline int rsMax(const int a, const int b) {
+  return a > b ? a : b;
+}
+
+extern inline int rsMin(const int a, const int b) {
+  return a < b ? a : b;
+}
+
 int main(int argc, char * argv[]) {
     
     FSLIO *fslio;
@@ -62,12 +93,17 @@ int main(int argc, char * argv[]) {
 	char *outputpath = NULL;
 	char *maskpath = NULL;
 	char *realignmentpath = NULL;
+	char *fdpath = NULL;
+	char *dvarspath = NULL;
+	char *flaggedpath = NULL;
 	
 	int x=-1, y=-1, z=-1, t=0;
 	short xDim, yDim, zDim, vDim;
 	short pixtype;
 	size_t dt;
     float inter = 0.0, slope = 1.0;
+
+	float fdthreshold=1.0, dvarsthreshold=0.05;
         
     BOOL verbose = FALSE;
     int threads = 1;
@@ -102,6 +138,24 @@ int main(int argc, char * argv[]) {
 				return 1;
 			}
 			maskpath = argv[ac];  /* no string copy, just pointer assignment */
+		} else if ( ! strcmp(argv[ac], "-dvars") ) {
+			if( ++ac >= argc ) {
+				fprintf(stderr, "** missing argument for -dvars\n");
+				return 1;
+			}
+			dvarspath = argv[ac];  /* no string copy, just pointer assignment */
+		} else if ( ! strcmp(argv[ac], "-fd") ) {
+			if( ++ac >= argc ) {
+				fprintf(stderr, "** missing argument for -fd\n");
+				return 1;
+			}
+			fdpath = argv[ac];  /* no string copy, just pointer assignment */
+		} else if ( ! strcmp(argv[ac], "-flagged") ) {
+			if( ++ac >= argc ) {
+				fprintf(stderr, "** missing argument for -flagged\n");
+				return 1;
+			}
+			flaggedpath = argv[ac];  /* no string copy, just pointer assignment */
 		} else if ( ! strncmp(argv[ac], "-v", 2) ) {
 			verbose = TRUE;
 		} else if ( ! strcmp(argv[ac], "-output") ) {
@@ -116,6 +170,18 @@ int main(int argc, char * argv[]) {
            		return 1;
            	}
            	threads = atoi(argv[ac]);  /* no string copy, just pointer assignment */
+        } else if ( ! strcmp(argv[ac], "-dvarsthreshold") ) {
+  			if( ++ac >= argc ) {
+           		fprintf(stderr, "** missing argument for -dvarsthreshold\n");
+           		return 1;
+           	}
+           	dvarsthreshold = atof(argv[ac]);  /* no string copy, just pointer assignment */
+        } else if ( ! strcmp(argv[ac], "-fdthreshold") ) {
+  			if( ++ac >= argc ) {
+           		fprintf(stderr, "** missing argument for -fdthreshold\n");
+           		return 1;
+           	}
+           	fdthreshold = atof(argv[ac]);  /* no string copy, just pointer assignment */
         } else {
 			fprintf(stderr, "\nError, unrecognized command %s\n", argv[ac]);
 		}
@@ -145,7 +211,18 @@ int main(int argc, char * argv[]) {
         fprintf(stdout, "Input file:  %s\n", inputpath);
         fprintf(stdout, "Mask file:   %s\n", maskpath);
         fprintf(stdout, "Output file: %s\n", outputpath);
+		fprintf(stdout, "DVARS threshold: %.4f\n", dvarsthreshold);
+		fprintf(stdout, "Framewise displacement threshold: %.4f\n", fdthreshold);
         fprintf(stdout, "Realignment parameters file: %s\n", realignmentpath);
+		
+		if ( dvarspath != NULL ) 
+			fprintf(stdout, "DVARS file: %s\n", dvarspath);
+		
+		if ( fdpath != NULL ) 
+			fprintf(stdout, "Framewise displacement file: %s\n", fdpath);
+		
+		if ( flaggedpath != NULL ) 
+			fprintf(stdout, "Flagged frames file: %s\n", flaggedpath);
     }
     
     // Load file
@@ -193,20 +270,6 @@ int main(int argc, char * argv[]) {
 	/* determine datatype */
 	dt = FslGetDataType(fslio, &pixtype);
     
-    /* prepare output file */    
-//    fslioOutput = FslOpen(outputpath, "wb");
-//    
-//    if (fslioOutput == NULL) {
-//        fprintf(stderr, "\nError, could not read header info for %s.\n", outputpath);
-//        return 1;
-//    }
-//    
-//    FslCloneHeader(fslioOutput, fslio);
-//    FslSetDim(fslioOutput, xDim, yDim, zDim, vDim);
-//    FslSetDimensionality(fslioOutput, 4);
-//    FslSetDataType(fslioOutput, pixtype);
-//    FslWriteHeader(fslioOutput);
-    
     // Prepare buffer
     buffsize       = (size_t)xDim*(size_t)yDim*(size_t)zDim*(size_t)vDim*(size_t)dt/(size_t)8;
     buffer         = malloc(buffsize);
@@ -218,15 +281,6 @@ int main(int argc, char * argv[]) {
 
     FslReadVolumes(fslio, buffer, vDim);
     
-	double signal[vDim];
-	double ***dataNow = d3matrix(zDim-1,  yDim-1, xDim-1);
-	rsExtractTimecourseFromBuffer(fslio, signal, buffer, slope, inter, MakePoint3D(50, 30, 20), xDim, yDim, zDim, vDim);
-    rsExtractVolumeFromBuffer(fslio, dataNow[0][0], buffer, slope, inter, t, xDim, yDim, zDim);
-	fprintf(stdout, "%d: %.5f\n", t, dataNow[50][30][20]);
-	fprintf(stdout, "%d: %.5f\n", t, signal[t]);
-	free(dataNow[0][0]);    free(dataNow[0]);	 free(dataNow);
-
-
 	/* load mask */
     unsigned long nPoints = 0L;
     double ***mask = d3matrix(zDim, yDim, xDim);
@@ -237,51 +291,184 @@ int main(int argc, char * argv[]) {
         FslClose(fslioOutput);
         return 1;
     }
+	
+	/* get value range */
+	double min=9999999999999999, max=-9999999999999999;
+	#pragma omp parallel num_threads(threads) private(t) shared(buffer,fslio)
+    {
+        #pragma omp for schedule(guided, 1)
+		for (t=5; t<vDim; t=t+1) {
+			double ***data = d3matrix(zDim-1,  yDim-1, xDim-1);
+			rsExtractVolumeFromBuffer(fslio, data[0][0], buffer, slope, inter, t, xDim, yDim, zDim);
+			
+			for (long p = 0L; p<nPoints; p=p+1L) {
+				const Point3D point  = maskPoints[p];
+				const double intensity = data[point.z][point.y][point.x];
+				
+				if ( intensity != intensity ) {
+					continue;
+				}
+				
+				if ( min > intensity ) {
+					min = intensity;
+				}
+				
+				if ( max < intensity ) {
+					max = intensity;
+				}
+			}
+			
+			free(data[0][0]); free(data[0]); free(data);
+		}
+	}
+	
+	fprintf(stdout, "min: %.2f, max: %.2f\n", min, max);
 
 	/* compute dvars */
 	double dvars[vDim];
 	dvars[0]=0;
-    //#pragma omp parallel num_threads(threads) private(t) shared(buffer,fslio,dvars)
-    //{
-    //    #pragma omp for schedule(guided, 1)
-//		for (t=1; t<2/*vDim*/; t=t+1) {
-//
-//			/* Extract a single volume for timepoint t from the buffer */
-//			double ***dataNow    = d3matrix(zDim-1,  yDim-1, xDim-1);
-//			double ***dataBefore = d3matrix(zDim-1,  yDim-1, xDim-1);
-//	        rsExtractVolumeFromBuffer(fslio, dataNow[0][0],    buffer, slope, inter, t,   xDim, yDim, zDim);
-//	        rsExtractVolumeFromBuffer(fslio, dataBefore[0][0], buffer, slope, inter, t-1, xDim, yDim, zDim);
-//			
-//			fprintf(stdout, "%d: %.5f\n", t, dataNow[50][30][20]);
-//			
-//			dvars[t]=0;
-//			
-//	        for (long p = 0L; p<nPoints; p=p+1L) {
-//				const Point3D point = maskPoints[p];
-//				//fprintf(stdout, "%d: X%03dY%03dZ%03d: %.3f - %.3f\n", t, point.x, point.y, point.z, dataNow[point.z][point.y][point.x], dataBefore[point.z][point.y][point.x]);
-//				dvars[t]=dvars[t] + pow(dataNow[point.z][point.y][point.x] - dataBefore[point.z][point.y][point.x], 2.0);
-//	        }
-//			
-//			//fprintf(stdout, "%d: %.4f\n", t, dvars[t]);
-//			dvars[t]=sqrt(dvars[t]);
-//	
-//			/* Free up memory */
-//			free(dataNow[0][0]);    free(dataNow[0]);	 free(dataNow);
-//			free(dataBefore[0][0]); free(dataBefore[0]); free(dataBefore);
-//		}
-    //}
+    #pragma omp parallel num_threads(threads) private(t) shared(buffer,fslio,dvars)
+    {
+        #pragma omp for schedule(guided, 1)
+		for (t=1; t<vDim; t=t+1) {
 
-	/*for ( t=0; t<vDim; t=t+1 ) {
-		fprintf(stdout, "%d: %.4f\n", t, dvars[t]);
-	}*/
-
+			/* Extract a single volume for timepoint t from the buffer */
+			double ***dataNow    = d3matrix(zDim-1,  yDim-1, xDim-1);
+			double ***dataBefore = d3matrix(zDim-1,  yDim-1, xDim-1);
+	        rsExtractVolumeFromBuffer(fslio, dataNow[0][0],    buffer, slope, inter, t,   xDim, yDim, zDim);
+	        rsExtractVolumeFromBuffer(fslio, dataBefore[0][0], buffer, slope, inter, t-1, xDim, yDim, zDim);
+			
+			dvars[t]=0;
+			
+	        for (long p = 0L; p<nPoints; p=p+1L) {
+				const Point3D point  = maskPoints[p];
+				const double INow    = (dataNow[point.z][point.y][point.x]    - min)/fabs(max-min);
+				const double IBefore = (dataBefore[point.z][point.y][point.x] - min)/fabs(max-min);
+				
+				//fprintf(stdout, "%d: X%03dY%03dZ%03d: %.3f - %.3f\n", t, point.x, point.y, point.z, IBefore, INow);
+				dvars[t]=dvars[t] + pow(INow - IBefore, 2.0);
+	        }
+			
+			//fprintf(stdout, "%d: %.4f\n", t, dvars[t]);
+			dvars[t]=sqrt(dvars[t] / (double)nPoints);
+			//fprintf(stdout, "%d: %.4f\n", t, dvars[t]);
 	
+			/* Free up memory */
+			free(dataNow[0][0]);    free(dataNow[0]);	 free(dataNow);
+			free(dataBefore[0][0]); free(dataBefore[0]); free(dataBefore);
+		}
+    }
+
+	if ( dvarspath != NULL ) {
+		
+		FILE *dvarsfile; 
+		dvarsfile = fopen(dvarspath,"w");
+		
+		for ( t=0; t<vDim; t=t+1 ) {
+			fprintf(dvarsfile, "%.10f\n", dvars[t]);
+		}
+		
+		fclose(dvarsfile);
+	}
+
+	if ( fdpath != NULL ) {
+		
+		FILE *fdfile; 
+		fdfile = fopen(fdpath,"w");
+		
+		for ( t=0; t<vDim; t=t+1 ) {
+			fprintf(fdfile, "%.10f\n", fd[t]);
+		}
+		
+		fclose(fdfile);
+	}
     
-    /* write output */
+	/* mark all frames as unflagged */
+	BOOL flaggedFrames[vDim];
+	
+	for ( t=0; t<vDim; t=t+1 ) {
+		flaggedFrames[t] = FALSE;
+	}
+	
+	/* determine which frames need to be flagged */
+	for ( t=1; t<vDim; t=t+1 ) {
+		if ( fd[t] > fdthreshold || dvars[t] > dvarsthreshold ) {
+			flaggedFrames[rsMax(t-1,0)]      = TRUE;
+			flaggedFrames[t]                 = TRUE;
+			flaggedFrames[rsMin(t+1,vDim-1)] = TRUE;
+		}
+	}
+	
+	if ( flaggedpath != NULL ) {
+		
+		FILE *flaggedfile; 
+		flaggedfile = fopen(flaggedpath,"w");
+		
+		for ( t=0; t<vDim; t=t+1 ) {
+			if ( flaggedFrames[t] == TRUE ) {
+				fprintf(flaggedfile, "%d\n", t);
+			}
+		}
+		
+		fclose(flaggedfile);
+	}
+	
+	/* count remaining frames */
+	int remainingFrames = 0;
+	int framemap[vDim];
+
+	for ( t=0; t<vDim; t=t+1 ) {
+		if ( flaggedFrames[t] == FALSE ) {
+			framemap[remainingFrames] = t;
+			remainingFrames = remainingFrames + 1;
+		}
+	}
+
+	/* prepare output file */
+	 
+    fslioOutput = FslOpen(outputpath, "wb");
+    
+    if (fslioOutput == NULL) {
+        fprintf(stderr, "\nError, could not read header info for %s.\n", outputpath);
+        return 1;
+    }
+    
+    FslCloneHeader(fslioOutput, fslio);
+    FslSetDim(fslioOutput, xDim, yDim, zDim, remainingFrames);
+    FslSetDimensionality(fslioOutput, 4);
+    FslSetDataType(fslioOutput, pixtype);
+    FslWriteHeader(fslioOutput);
+
+   	/* create resulting file */
+	buffsize     = (size_t)xDim*(size_t)yDim*(size_t)zDim*(size_t)remainingFrames*(size_t)dt/(size_t)8;
+    outputBuffer = malloc(buffsize);
+        
+    if (outputBuffer == NULL) {
+        fprintf(stderr, "Not enough free memory :-(\n");
+        return 1;
+    }
+
+    #pragma omp parallel num_threads(threads) private(t) shared(flaggedFrames, fslio, buffer, outputBuffer)
+    {
+        #pragma omp for schedule(guided, 1)
+		for ( t=0; t<remainingFrames; t=t+1 ) {
+					
+			/* Extract a single volume for timepoint t from the buffer */
+			double ***data = d3matrix(zDim-1,  yDim-1, xDim-1);
+            rsExtractVolumeFromBuffer(fslio, data[0][0], buffer, slope, inter, framemap[t], xDim, yDim, zDim);
+
+			/* Write back to the buffer */
+            rsWriteVolumeToBuffer(fslioOutput, data[0][0], outputBuffer, slope, inter, t, xDim, yDim, zDim);
+
+			/* Free up memory */
+			free(data[0][0]); free(data[0]); free(data);
+		}
+	}
+
     free(buffer);
-    /*FslWriteVolumes(fslioOutput, outputBuffer, vDim);
+    FslWriteVolumes(fslioOutput, outputBuffer, remainingFrames);
     FslClose(fslioOutput);
+    free(fslioOutput);
     free(outputBuffer);
-    free(fslioOutput);*/
     free(maskPoints);
 }
